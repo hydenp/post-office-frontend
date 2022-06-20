9cd4be7f4922983c9d90b7b22c7fc0b033d33396
Hyden Polikoff <hyden.dev@gmail.com>
2022-06-20 12:57:43 -0600

    basic working flow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+import jwt_decode from 'jwt-decode'
 
 function App() {
+
+  const [user, setUser] = useState({});
+
+  function handleCallbackResponse(response) {
+    console.log(response);
+    const decoded = jwt_decode(response.credential);
+    console.log(decoded);
+    setUser(decoded);
+    console.log(user)
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: '414346990222-dgm0rbabpsi9ldrpn3k6fqu2dgj9safb.apps.googleusercontent.com',
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById('signInDiv'),
+      {theme: 'outline', size: 'large'}
+    )
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
     </div>
   );
 }
