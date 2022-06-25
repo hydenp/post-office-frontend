3199f1e2b603b5f05614e0ef8a126ba633b25932
Hyden Polikoff <hyden.dev@gmail.com>
2022-06-25 15:19:02 -0600

    basic oauth flow for Google OAuth

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import './App.css';
-import {useEffect, useState} from 'react'
-import jwt_decode from 'jwt-decode'
+
+import GoogleOauth from './components/GoogleAuth'
 
 function App() {
 
-  const [user, setUser] = useState({});
-
-  function handleCallbackResponse(response) {
-    console.log(response);
-    const decoded = jwt_decode(response.credential);
-    console.log(decoded);
-    setUser(decoded);
-    console.log(user)
-  }
-
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: '414346990222-dgm0rbabpsi9ldrpn3k6fqu2dgj9safb.apps.googleusercontent.com',
-      callback: handleCallbackResponse
-    });
-
-    google.accounts.id.renderButton(
-      document.getElementById('signInDiv'),
-      {theme: 'outline', size: 'large'}
-    )
-  }, [])
-
-
   return (
     <div className="App">
-      <div id="signInDiv"></div>
+
+      {/* Component to start google oauth flow, store it in state variable and print token */}
+      <GoogleOauth/>
     </div>
   );
 }
